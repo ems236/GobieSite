b6765b5a4882b7d6caaf994ed40a7fe8dcb2b382
Ellis <ems236@case.edu>
2019-08-04 19:02:26 -0400

    added more pages

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,19 @@ from app import app
 @app.route('/index')
 def index():
     return render_template("home.html")
+
+@app.route('/social')
+def social():
+	return render_template("social.html")
+
+@app.route('/calendar')
+def calendar():
+	return render_template("calendar.html")
+
+@app.route('/gum')
+def gum():
+	return render_template("gum.html")
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
