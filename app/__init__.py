ea4dab4d3ccaf7b3d397650876f3671b6f38d979
Ellis <ems236@case.edu>
2019-08-19 18:14:12 -0400

    fixed import error

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://webappuser:scwruHead2@localhost/Gobies'
