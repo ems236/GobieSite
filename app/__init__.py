68ec06887c5ed0d16b56a8734b5633222b062e05
Ellis <ems236@case.edu>
2019-08-18 22:55:02 -0400

    setup for flask sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from models import db
 
 app = Flask(__name__)
+db.init_app(app)
 
 from app import routes
---
+++ app/models.py
@@ -0,0 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+team_player = db.Table(
+	'team_player',
+	db.Column('teamId', db.Integer, primary_key=True, db.ForeignKey('team.id')),
+	db.Column('playerId', db.Integer, primary_key=True, db.ForeignKey('player.id')),
+	db.Column('nickname', db.String),
+	db.Column('position', db.String),
+	)
+
+class TeamType(db.Model):
+	__tablename__ = 'teamType'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String)
+	teams = db.relationship("Team", backref="type", lazy=False)
+
+class Team(db.Model):
+	__tablename__ = 'team'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String)
+	imageFileName = db.Column(db.String)
+	springYear = db.Column(db.Integer)
+	type = db.Column(db.Integer, db.ForeignKey('teamType.id'))
+	players = db.relationship("Player", secondary='team_player', lazy='subquery', backref=db.backref('teams', lazy=True))
+
+class Player(db.Model):
+	__tablename__ = 'player'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String)
+	hometown = db.Column(db.String)
+	gradYear = db.Column(db.String)
+	major = db.Column(db.String)
+
