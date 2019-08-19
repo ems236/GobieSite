c2eeedda8f33f6bb43f3a5e7c18fc192a37c9726
Ellis <ems236@case.edu>
2019-08-19 19:22:04 -0400

    trying an aggregate again

--- app/models.py
+++ app/models.py
@@ -25,7 +25,7 @@ class Team(db.Model):
 	players = db.relationship("Player", secondary='team_player', lazy='subquery', backref=db.backref('teams', lazy=True))
 
 	def currentOfType(queryTypeId):
-		maxYear = db.session.query(func.max(Team.springYear)).scalar()
+		maxYear = db.session.query(db.func.max(Team.springYear)).filter(Team.typeId == queryTypeId).scalar()
 		return getByTypeYear(queryTypeId, maxYear)
 
 	def getByTypeYear(queryTypeId, year):
